1eda96bdce8ae1ba6cc9498e3c7bb381ba28ee03
MMikey <michael.mulgrew@hotmail.com>
2022-04-06 17:38:41 +0100

    created models and began setting up api in django

--- backend/nlidb/admin.py
+++ backend/nlidb/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
-
+from .models import MedicalStaff, Patients, Appointments, Treatments
 # Register your models here.
+
+class MedicalStaffAdmin(admin.ModelAdmin):
+    list_display = ('staff_id','name', 'salary', 'date_hired', 'is_working')
+
+class PatientsAdmin(admin.ModelAdmin):
+    list_display = ('patient_id','name', 'DOB', 'NHS_num', 'BMI', 'admitted')
+
+class AppointmentsAdmin(admin.ModelAdmin):
+    list_display = ('patient', 'staff', 'description', 'date_time')
+
+class TreatmentsAdmin(admin.ModelAdmin):
+    list_display = ('patient', 'staff', 'cost', 'description')
+
+
+admin.site.register(MedicalStaff, MedicalStaffAdmin)
+admin.site.register(Patients, PatientsAdmin)
+admin.site.register(Appointments, AppointmentsAdmin)
+admin.site.register(Treatments, TreatmentsAdmin)
+
