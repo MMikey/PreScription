7ad1003aa83edb59232337bec1e701eb72dafbcb
MMikey <michael.mulgrew@hotmail.com>
2022-04-17 14:47:32 +0100

    minor changes

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,9 +4,13 @@ from rest_framework import viewsets
 from rest_framework.decorators import action
 from .serializers import TranslationSerializer
 from .models import Translation
+from django.db import transaction
+
+
 
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+from nltk.tokenize.treebank import TreebankWordDetokenizer
 
 # Create your views here.
 
@@ -14,10 +18,9 @@ class TranslationView(viewsets.ModelViewSet):
     serializer_class = TranslationSerializer
     queryset = Translation.objects.all()
 
-    def create():
+    def create(self, request):
+        return self.remove_stopwords(request)
 
-        
-    @action(detail=True, methods=['post'])
     def remove_stopwords(self, request):
  
         question = request.data['nl_question']
@@ -27,7 +30,8 @@ class TranslationView(viewsets.ModelViewSet):
 
         request.data['nl_question'] = tokens_without_sw
 
-        return Response({'status':'stop words removed'})
+        handler500 = 'error'
+        return Response({'nl_question':TreebankWordDetokenizer().detokenize(request.data['nl_question'])})
 
 
 
